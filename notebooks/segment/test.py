d1542b538ac709ad330b501caa1815327148e380
Taiyi Chen <taiyic2@uw.edu>
2026-05-08 20:45:35 -0700

    update segment files

--- notebooks/segment/test.py
+++
@@ -1,7 +0,0 @@
-from transformers import MT5Tokenizer
-import torch
-import torch.nn.functional as F
-from transformers import AutoTokenizer, AutoModel
-from collections import Counter
-
-print('1')
