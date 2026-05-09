13eb6e11b0558afdac9b12900b99f28d6274153b
Taiyi Chen <taiyic2@uw.edu>
2026-05-08 20:34:25 -0700

    add data segmentation demo

---
+++ notebooks/segment/test.py
@@ -0,0 +1,12 @@
+# from sentence_transformers import SentenceTransformer
+
+print('ok')
+
+# from transformers import MT5Tokenizer
+
+# tokenizer = MT5Tokenizer.from_pretrained("google/mt5-small")
+
+# text = "Some text here"
+
+# tokens = tokenizer.encode(text)
+# print(len(tokens))
